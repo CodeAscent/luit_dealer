0a1c754221466088fe334212269b6777023d76eb
itsHarshdeepSingh <harshsinghsaluja10@gmail.com>
2025-01-06 23:13:00 +0530

    bug resolved

--- lib/features/auth/view/screens/login_screen.dart
+++ lib/features/auth/view/screens/login_screen.dart
@@ -45,135 +45,131 @@ class _LoginScreenState extends State<LoginScreen> {
         autovalidateMode: validationType,
         key: formKey,
         child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset(
-                  'assets/Images/LUIT_page2_image1.png',
-                  height: 300,
-                ),
-                //   Spacer(),
-                Text(
-                  'Login ',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset(
+                    'assets/Images/LUIT_page2_image1.png',
+                    height: 300,
                   ),
-                ),
-                SizedBox(height: 10),
-                CustomTextField(
-                  controller: phoneController,
-                  validator: (val) {
-                    if (val!.length < 10) {
-                      return 'Enter a valid number';
-                    }
-                    return null;
-                  },
-                  hint: 'Enter phone number',
-                  maxLength: 10,
-                  type: TextInputType.phone,
-                ),
-                SizedBox(height: 20),
-
-                CustomTextField(
-                  suffix: IconButton(
+                  Text(
+                    'Login ',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    controller: phoneController,
+                    validator: (val) {
+                      if (val!.length < 10) {
+                        return 'Enter a valid number';
+                      }
+                      return null;
+                    },
+                    hint: 'Enter phone number',
+                    maxLength: 10,
+                    type: TextInputType.phone,
+                  ),
+                  SizedBox(height: 20),
+                  CustomTextField(
+                    suffix: IconButton(
                       onPressed: () {
-                        showPassword = !showPassword;
-                        setState(() {});
+                        setState(() {
+                          showPassword = !showPassword;
+                        });
                       },
                       icon: Icon(showPassword
                           ? Icons.visibility
-                          : Icons.visibility_off)),
-                  isPassword: !showPassword,
-                  controller: passwordController,
-                  validator: (val) {
-                    if (val == '') {
-                      return 'Enter a valid password';
-                    }
-                    return null;
-                  },
-                  hint: 'Enter password',
-                  maxLength: 10,
-                  // type: TextInputType.phone,
-                ),
-
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
+                          : Icons.visibility_off),
+                    ),
+                    isPassword: !showPassword,
+                    controller: passwordController,
+                    validator: (val) {
+                      if (val == '') {
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    },
+                    hint: 'Enter password',
+                    maxLength: 10,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
                         onPressed: () {
                           Get.to(() => ForgotPasswordReqOtp());
                         },
-                        child: Text('Forgot password?')),
-                  ],
-                ),
-                SizedBox(height: 20),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Dealer'),
-                    SizedBox(width: 5),
-                    CupertinoCheckbox(
+                        child: Text('Forgot password?'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Dealer'),
+                      SizedBox(width: 5),
+                      CupertinoCheckbox(
                         activeColor: AppColors.primaryColor,
                         value: !isSalesLogin,
                         onChanged: (val) {
                           setState(() {
                             isSalesLogin = !isSalesLogin;
                           });
-                        }),
-                    Text('Sales'),
-                    SizedBox(width: 5),
-                    CupertinoCheckbox(
+                        },
+                      ),
+                      Text('Sales'),
+                      SizedBox(width: 5),
+                      CupertinoCheckbox(
                         activeColor: AppColors.primaryColor,
                         value: isSalesLogin,
                         onChanged: (val) {
                           setState(() {
                             isSalesLogin = !isSalesLogin;
                           });
-                        }),
-                  ],
-                ),
-
-                SizedBox(height: 20),
-                Obx(
-                  () => CustomOrangeButtom(
-                    loading: loginViewModel.loading.value,
-                    label: 'Submit',
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        // await AuthLocalRepo().saveToken(
-                        //     '1150|LdWSifZbAIXHfQaEfdUsnKy9FNQID9Y8AOLXqjsY2a81ca1a');
-
-                        // Get.to(() => BottomNavigationScreen());
-                        //
-                        LocalStorage.sharedPreferences
-                            .setBool('isSales', isSalesLogin);
-                        loginViewModel.login(
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Obx(
+                        () => CustomOrangeButtom(
+                      loading: loginViewModel.loading.value,
+                      label: 'Submit',
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          LocalStorage.sharedPreferences.setBool('isSales', isSalesLogin);
+                          loginViewModel.login(
                             number: phoneController.text,
                             password: passwordController.text,
-                            salesOrDealer: isSalesLogin ? 'sales' : 'dealer');
-                      } else {
-                        validationType = AutovalidateMode.onUserInteraction;
-                      }
-                      setState(() {});
-                    },
+                            salesOrDealer: isSalesLogin ? 'sales' : 'dealer',
+                          );
+                        } else {
+                          setState(() {
+                            validationType = AutovalidateMode.onUserInteraction;
+                          });
+                        }
+                      },
+                    ),
                   ),
-                ),
-                Spacer(),
-                Text(
-                  'By login, you agree to our Privacy Policy and Terms & Conditions.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black54,
+                  SizedBox(height: 10),
+                  Text(
+                    'By login, you agree to our Privacy Policy and Terms & Conditions.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black54,
+                    ),
                   ),
-                ),
-                SizedBox(height: 16),
-              ],
+                ],
+              ),
             ),
           ),
         ),
